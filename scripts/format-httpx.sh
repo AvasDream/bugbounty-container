628fb6a6689cba32b37721ae8254520a95f2c7d9
vgrimmeisen <vincent.grimmeisen@redguard.ch>
2020-11-03 11:40:40 +0100

    Set correct owner

--- scripts/format-httpx.sh
+++ scripts/format-httpx.sh
@@ -3,6 +3,7 @@ for i in $(ls -d */);
 do
 domain=$(echo $i | cut -d "/" -f1)
 cd $i
+sudo chown -R ubuntu $i
 if [[ -f httpx-$domain.canary ]]
 then
         cat httpx-$domain.txt | cut -d " " -f 1 | cut -d":" -f1,2 > online-subdomains.txt
