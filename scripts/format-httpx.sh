fc75b001a13e92cd2aca94f1a7819f7b729e8674
vgrimmeisen <vincent.grimmeisen@redguard.ch>
2020-11-03 11:38:52 +0100

    Added format script for httpx

---
+++ scripts/format-httpx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for i in $(ls -d */);
+do
+domain=$(echo $i | cut -d "/" -f1)
+cd $i
+if [[ -f httpx-$domain.canary ]]
+then
+        cat httpx-$domain.txt | cut -d " " -f 1 | cut -d":" -f1,2 > online-subdomains.txt
+fi
+cd ..
+done
