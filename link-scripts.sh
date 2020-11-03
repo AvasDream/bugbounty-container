a1659b2b233ab262676c2305c9d8b71483c7ad0a
vgrimmeisen <vincent.grimmeisen@redguard.ch>
2020-11-03 09:36:10 +0100

    Clean up functionality for link-scripts.sh

--- link-scripts.sh
+++ link-scripts.sh
@@ -2,6 +2,10 @@
 
 for i in $(ls scripts/);
 do 
+    if [ -f /usr/local/bin/$i ]
+    then
+        sudo rm -rf /usr/local/bin/$i
+    fi
     chmod +x "scripts/$i" 
     sudo ln -s "$(pwd)/scripts/$i" /usr/local/bin/
 done
