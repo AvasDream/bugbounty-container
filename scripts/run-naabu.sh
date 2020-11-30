0149a4e74d114b75dc9792bab532094e4971a5b3
vgrimmeisen <vincent.grimmeisen@redguard.ch>
2020-11-30 12:17:29 +0100

    Error in logic

--- scripts/run-naabu.sh
+++ scripts/run-naabu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
+set -e
 for i in $(ls -d */);
 do
 domain=$(echo $i | cut -d "/" -f1)
 cd $i
 mkdir portscan
-touch naabu-$domain.canary
 if [[ -f naabu-$domain.canary ]]
 then
         echo "Naabu was executed. Skipping $domain"
@@ -14,5 +14,6 @@ else
         sudo docker run -it --rm projectdiscovery/naabu -host $domain -p- -silent -nC > /portscan/ports-$domain.txt
         notify "Port scan: $domain - $(cat "/portscan/ports-$domain.txt")"
 fi
+touch naabu-$domain.canary
 cd ..
 done
