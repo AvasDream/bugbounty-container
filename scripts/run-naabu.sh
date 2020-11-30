8040e1ae12e9562592ab7d3a95099daadd0614c7
vgrimmeisen <vincent.grimmeisen@redguard.ch>
2020-11-30 12:30:01 +0100

    Naabu iterate over all domains not only the parent one.

--- scripts/run-naabu.sh
+++ scripts/run-naabu.sh
@@ -5,14 +5,18 @@ do
 domain=$(echo $i | cut -d "/" -f1)
 cd $i
 mkdir portscan
+current_dir=$(pwd)
 if [[ -f naabu-$domain.canary ]]
 then
         echo "Naabu was executed. Skipping $domain"
         sleep 1
 else
-        echo $domain
-        sudo docker run -it --rm projectdiscovery/naabu -host $domain -p - -silent -nC > /portscan/ports-$domain.txt
-        notify "Port scan: $domain - $(cat "/portscan/ports-$domain.txt")"
+        for target in $(cat online-naabu.txt);
+        do
+                echo $target
+                sudo docker run -it --rm projectdiscovery/naabu -host $target -p - -silent -nC > "$current_dir/portscan/ports-$target.txt"
+                notify "Port scan: $target - $(cat "$current_dir/portscan/ports-$target.txt")"
+        done
 fi
 touch naabu-$domain.canary
 cd ..
