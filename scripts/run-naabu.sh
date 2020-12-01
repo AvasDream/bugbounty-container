27eabca9339e541cf3a286c9c18c7eef1ed518c5
vgrimmeisen <vincent.grimmeisen@redguard.ch>
2020-12-01 14:27:05 +0100

    Naabu correct loops

--- scripts/run-naabu.sh
+++ scripts/run-naabu.sh
@@ -4,17 +4,17 @@ for i in $(ls -d */);
 do
 domain=$(echo $i | cut -d "/" -f1)
 cd $i
-current_dir=$(pwd)
 if [[ -d portscan ]]
 then
         echo "Naabu was executed. Skipping $domain"
         sleep 1
 else
-
+        echo "Execute for parent domain $i"
+        current_dir=$(pwd)
         mkdir portscan
         for target in $(cat online-naabu.txt);
         do
-                echo $target
+                echo "Host: $target"
                 sudo docker run -it --rm projectdiscovery/naabu -host $target -p - -silent -nC > "$current_dir/portscan/ports-$target.txt"
                 notify "Port scan: $target - $(cat "$current_dir/portscan/ports-$target.txt")"
         done
